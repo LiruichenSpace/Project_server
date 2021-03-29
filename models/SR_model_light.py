57d428a3e19b164fc10af4fc3bab04d90226d45c
lrch0101@163.com <lrch0101@163.com>
2021-03-29 15:58:01 +0800

    add light SR model support

---
+++ models/SR_model_light.py
@@ -0,0 +1,62 @@
+import copy
+import logging
+import time
+
+import torch
+
+from models.modules import loss
+from models.modules.IMDN.architecture import IMDN
+
+class SR_model_light():
+    def __init__(self, model_path, learning_rate=0.001, update_interval=5):
+        if torch.cuda.is_available():
+            self.pred_device = torch.device('cuda:0')  # used for one GPU
+            self.learn_device = torch.device('cuda:0')
+            self.multi_gpu_flag = False
+        else:
+            self.pred_device = torch.device('cpu')
+            self.learn_device = torch.device('cpu')
+            self.multi_gpu_flag = False
+        # 根据GPU情况初始化模型Done
+        self.logger = logging.getLogger('base')  # 获取logger
+        self.update_interval = update_interval
+        self.model_update_time = None  # 记录上次更新时间，过了一定时长后更新
+        # 定义模型更新参数Done
+        model = IMDN(upscale=4)
+        model.load_state_dict(torch.load(str(model_path)), strict=True)
+        self.learn_model = copy.deepcopy(self.pred_model)
+        self.learn_model = self.learn_model.to(self.learn_device)
+        self.pred_model = model.to(self.pred_device)
+        # self.pred_model=torch.nn.DataParallel(model,device_ids=[0,1])
+        self.pred_model.eval()
+        self.learn_model.train()
+        # 定义模型Done
+        self.critic = loss.CharbonnierLoss().to(self.learn_device)
+        self.learning_rate = learning_rate
+        self.optimizer = torch.optim.Adam(self.learn_model.parameters(), self.learning_rate)
+        # 定义optimizer和损失函数
+
+    def learning(self, data):
+        self.learn_model.train()
+        self.optimizer.zero_grad()
+        output = self.learn_model(data['LQs'].to(self.learn_device))
+        loss = self.critic(output, data['GT'].to(self.learn_device))
+        self.logger.info('time: {} ;loss is: {}'.format(data['key'], loss.cpu().item()))
+        loss.backward()
+        self.optimizer.step()
+        if self.model_update_time is None:
+            self.model_update_time = time.time()
+        elif time.time() - self.model_update_time > self.update_interval:
+            self.update_model()
+            self.model_update_time = time.time()
+        return loss
+
+    def update_model(self):
+        self.pred_model.load_state_dict(self.learn_model.state_dict())
+
+    def forward(self, data):
+        self.pred_model.eval()
+        data=data.squeeze(dim=0)
+        output = self.pred_model(data.to(self.pred_device))
+        output=output.unsqueeze(0)
+        return output
